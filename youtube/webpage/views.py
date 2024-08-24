6817b3c3a2a6497d7a3e3be23acb914665c2bbaa
MeSonali <bhumikag567@gmail.com>
2024-08-24 23:06:40 +0530

    trying to run my html file

--- youtube/webpage/views.py
+++ youtube/webpage/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    if request.method == "GET":
+        return render(request, 'index.html')
+    if request.method == 'POST':
+        pass
