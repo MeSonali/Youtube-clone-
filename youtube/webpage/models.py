e321a95bae65c263eae196ae0aa5e96e89b9b203
MeSonali <bhumikag567@gmail.com>
2024-08-27 17:58:03 +0530

    almost home page is made

--- youtube/webpage/models.py
+++ youtube/webpage/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Channel(models.Model):
+    name = models.CharField(max_length=255)
+    logo = models.ImageField(upload_to='channel_logo/')
+    owner = models.ForeignKey(User, on_delete=models.CASCADE) 
+
+    def __str__(self):
+        return self.name
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    video_file = models.FileField(upload_to='videos/')
+    thumbnail = models.ImageField(upload_to='thumbnail/', default=None)
+    views = models.IntegerField(default=0)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    likes = models.IntegerField(default=0, blank=True)
+    channel = models.ForeignKey(Channel, on_delete=models.CASCADE, related_name='videos')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='videos')  
+
+    def __str__(self):
+        return self.title
+
+    
+
+
+
+    
+    # likes = models.ManyToManyField(User, related_name='liked_videos', blank=True)
+    # channel = models.ForeignKey('Channel', on_delete=models.CASCADE)
+
--- youtube/webpage/views.py
+++ youtube/webpage/views.py
@@ -1,9 +1,44 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import Video,Channel
+from django.shortcuts import render, get_object_or_404,redirect
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 def home(request):
     if request.method == "GET":
-        return render(request, 'index.html')
+        # return render(request, 'video_feed.html')
+        pass
     if request.method == 'POST':
+        pass
+
+
+def video_feed(request):
+    if request.method == "GET":
+        videos = Video.objects.all()
+        context ={
+            'videos': videos
+            }
+       
+        return render(request, 'video_feed.html',context )
+    if request.method == 'POST':
+        pass
+
+def video_detail(request, pk):
+    if request.method == "GET":
+        video = get_object_or_404(Video, pk=pk)
+        context = {'video': video}
+        return render(request, 'video_detail.html', context)
+    if request.method == "POST":
+        pass
+
+
+def channel_detail(request, pk):
+    if request.method == "GET":
+        channel = get_object_or_404(Channel, pk=pk)
+        videos = channel.videos.all()  
+        context =  {'channel': channel, 'videos': videos}
+        return render(request, 'channel_detail.html',context)
+    if request.method == "POST":
         pass
