509cb805712d70c347505c822d5c87f2c95aac65
Fareed <fareedsayed95@gmail.com>
2024-09-01 13:14:29 +0530

    $commit_message

--- git_execute_windows.cmd
+++ git_execute_windows.cmd
@@ -1,14 +1,28 @@
-#!bin/bash
+#!/bin/bash
 ############################################
-#AUTHOR : FAREED SAYED
-#DATE : 01 SEPTEMBER 2024 
-#TO EXECUTE PROPERE GITHUB 
+# AUTHOR : FAREED SAYED
+# DATE : 01 SEPTEMBER 2024 
+# TO EXECUTE PROPER GITHUB 
 ############################################
 
+# Prompt for commit message
+echo "Enter commit message:"
+read commit_message
 
-git add . 
-git commit -m "notepad updated"
-git pull origin main 
-git add . 
-git commit -m "notepad updated"
+# Add changes to staging
+git add .
+
+# Commit changes with user-provided message
+git commit -m "$commit_message"
+
+# Pull latest changes from origin main
+git pull origin main
+
+# Add changes to staging again (in case there were updates from pull)
+git add .
+
+# Commit changes again with user-provided message
+git commit -m "$commit_message"
+
+# Push changes to origin branch named fareed
 git push origin fareed
