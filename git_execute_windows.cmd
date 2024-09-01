08fa3ac73463666e20fbf753ef609dd87253cf55
Fareed <fareedsayed95@gmail.com>
2024-09-01 13:17:16 +0530

    notepad with proper commit message updated

--- git_execute_windows.cmd
+++ git_execute_windows.cmd
@@ -4,25 +4,35 @@
 # DATE : 01 SEPTEMBER 2024 
 # TO EXECUTE PROPER GITHUB 
 ############################################
+@echo off
+setlocal
 
-# Prompt for commit message
-echo "Enter commit message:"
-read commit_message
+:: Prompt for commit message
+set /p commit_message="Enter commit message: "
 
-# Add changes to staging
+:: Check if the user provided a commit message
+if "%commit_message%"=="" (
+    echo Commit message cannot be empty. Exiting.
+    exit /b 1
+)
+
+:: Add changes to staging
 git add .
 
-# Commit changes with user-provided message
-git commit -m "$commit_message"
+:: Commit changes with user-provided message
+git commit -m "%commit_message%"
 
-# Pull latest changes from origin main
+:: Pull latest changes from origin main
 git pull origin main
 
-# Add changes to staging again (in case there were updates from pull)
+:: Add changes to staging again (in case there were updates from pull)
 git add .
 
-# Commit changes again with user-provided message
-git commit -m "$commit_message"
+:: Commit changes again with user-provided message
+git commit -m "%commit_message%"
 
-# Push changes to origin branch named fareed
+:: Push changes to origin branch named fareed
 git push origin fareed
+
+endlocal
+
